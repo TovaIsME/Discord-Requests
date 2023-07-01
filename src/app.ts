449a9d3b6d1316833133b440eca3c365465206e8
TovaTheCode <uglyr3b+github@gmail.com>
2023-06-30 22:22:03 -0800

    new channels and guild sorts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import axios from 'axios';
+import axios, {AxiosResponse} from 'axios';
 const app = express()
 const PORT = 3000 || process.env.PORT
 
@@ -83,6 +83,161 @@ app.get('/typing/:token/:channelId/:time', (req: Request, res: Response) => {
       res.send('POST request sent to Discord API');
   });
 
+  app.get('/GuildsAndChannels/sort/:token', async (req: Request, res: Response) => {
+    const { token } = req.params;
+    if (!token) return res.send('No Token Bad REQUEST');
+  
+    interface Guild {
+      id: string;
+      name: string;
+    }
+  
+    interface Channel {
+      id: string;
+      name: string;
+    }
+  
+    const headers = {
+      Authorization: token,
+    };
+  
+    let delay = 300;
+  
+    try {
+      let guildsResponse: AxiosResponse<Guild[]>;
+      let guilds: Guild[];
+  
+      do {
+        if (delay > 0) {
+          await new Promise(resolve => setTimeout(resolve, delay));
+        }
+  
+        guildsResponse = await axios.get('https://discord.com/api/v10/users/@me/guilds', { headers });
+        guilds = guildsResponse.data;
+  
+        if (guildsResponse.headers['retry-after']) {
+          delay = parseInt(guildsResponse.headers['retry-after']) * 1000;
+        }
+      } while (guildsResponse.status === 429); 
+  
+      const sortedGuilds = guilds.sort((a, b) => a.name.localeCompare(b.name));
+  
+      const sortedGuildsAndChannels: Record<string, Channel[]> = {};
+  
+      for (const guild of sortedGuilds) {
+        const guildId = guild.id;
+  
+        let channelsResponse: AxiosResponse<Channel[]>;
+        let channels: Channel[];
+  
+        do {
+          if (delay > 0) {
+            await new Promise(resolve => setTimeout(resolve, delay));
+          }
+  
+          channelsResponse = await axios.get(`https://discord.com/api/v10/guilds/${guildId}/channels`, { headers });
+          channels = channelsResponse.data;
+  
+          if (channelsResponse.headers['retry-after']) {
+            delay = parseInt(channelsResponse.headers['retry-after']) * 1000;
+          }
+        } while (channelsResponse.status === 429);
+  
+        const sortedChannels = channels.sort((a, b) => a.name.localeCompare(b.name));
+        sortedGuildsAndChannels[guild.name] = sortedChannels;
+      }
+  
+      res.json(sortedGuildsAndChannels);
+    } catch (error) {
+      console.error('Error occurred:', error);
+      res.status(500).send('This error might be caused by discord api Reason : Rate Limit, However You Can Try Another Request After 0.5 seconds');
+    }
+  });
+  app.get('/guilds/sort/:token', async (req: Request, res: Response) => {
+    const { token } = req.params;
+    if (!token) return res.send('No Token Bad REQUEST');
+  
+    interface Guild {
+      id: string;
+      name: string;
+    }
+  
+    const headers = {
+      Authorization: token,
+    };
+  
+    let delay = 300;
+  
+    try {
+      let guildsResponse: AxiosResponse<Guild[]>;
+      let guilds: Guild[];
+  
+      do {
+        if (delay > 0) {
+          await new Promise(resolve => setTimeout(resolve, delay));
+        }
+  
+        guildsResponse = await axios.get('https://discord.com/api/v10/users/@me/guilds', { headers });
+        guilds = guildsResponse.data;
+  
+        if (guildsResponse.headers['retry-after']) {
+          delay = parseInt(guildsResponse.headers['retry-after']) * 1000;
+        }
+      } while (guildsResponse.status === 429);
+  
+      const sortedGuilds = guilds.sort((a, b) => a.name.localeCompare(b.name));
+  
+      res.json(sortedGuilds);
+    } catch (error) {
+      console.error('Error occurred:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+  
+  app.get('/channels/sort/:token/:guildId', async (req: Request, res: Response) => {
+    const { guildId, token } = req.params;
+    if (!guildId) return res.send('No Guild ID Bad REQUEST');
+    if (!token) return res.send('No token Bad REQUEST')
+  
+    interface Channel {
+      id: string;
+      name: string;
+    }
+  
+    const headers = {
+      Authorization: token,
+    };
+  
+    let delay = 300;
+  
+    try {
+      let channelsResponse: AxiosResponse<Channel[]>;
+      let channels: Channel[];
+  
+      do {
+        if (delay > 0) {
+          await new Promise(resolve => setTimeout(resolve, delay));
+        }
+  
+        channelsResponse = await axios.get(`https://discord.com/api/v10/guilds/${guildId}/channels`, { headers });
+        channels = channelsResponse.data;
+  
+        if (channelsResponse.headers['retry-after']) {
+          delay = parseInt(channelsResponse.headers['retry-after']) * 1000;
+        }
+      } while (channelsResponse.status === 429);
+  
+      const sortedChannels = channels.sort((a, b) => a.name.localeCompare(b.name));
+  
+      res.json(sortedChannels);
+    } catch (error) {
+      console.error('Error occurred:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+  
+  
+  
 
 app.listen(PORT, () => {
     console.log('app is working on', PORT)
